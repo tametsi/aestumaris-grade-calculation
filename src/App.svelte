6d513afa9068868a987a9ee90c1fd80f3f69f214
Vis17 <93092155+Vis17@users.noreply.github.com>
2022-01-27 13:32:49 +0100

    Add card style

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,29 @@
 </script>
 
 <main>
-	<h1>AESTUMARIS - Grade Calculation</h1>
+	<div class="card">
+		<h1>AESTUMARIS - Grade Calculation</h1>
+
+		<p>
+			AESTUMARIS is a little tool to calculate grades based on the points,
+			reached in a classtest.
+		</p>
+	</div>
 </main>
 
 <style lang="scss">
-	//
+	@use './scss/abstracts' as a;
+	main {
+		padding: 1rem 0;
+
+		.card {
+			width: clamp(30rem, 75rem, 95%);
+			background-color: a.$gray6;
+			margin: auto;
+			padding: 1.5rem;
+
+			border-radius: 0.5rem;
+			box-shadow: darken(a.$gray6, 10) 0 0 1.5rem;
+		}
+	}
 </style>
