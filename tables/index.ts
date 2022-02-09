f3a9c103dfd24dd34af60d8abefd4262d5550409
Vis17 <93092155+Vis17@users.noreply.github.com>
2022-02-09 10:10:39 +0100

    Refactor: move `table.ts` in seperate folder

--- tables/index.ts
+++ tables/index.ts
@@ -1,5 +1,5 @@
-import type { TableJson } from '../src/lib/aestumaris/table';
-import Table from '../src/lib/aestumaris/table';
+import type { TableJson } from '../src/lib/table/table';
+import Table from '../src/lib/table/table';
 import table01 from './table_01.json';
 
 const tables: TableJson[] = [table01];
