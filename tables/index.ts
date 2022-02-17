65a19983c7a2a5defdb735e2db665c62ac4c2019
Vis17 <93092155+Vis17@users.noreply.github.com>
2022-02-17 09:55:04 +0100

    Add 2 more calculation tables

--- tables/index.ts
+++ tables/index.ts
@@ -1,8 +1,10 @@
 import type { TableJson } from '../src/lib/table/table';
 import Table from '../src/lib/table/table';
 import table01 from './table_01.json';
+import table02 from './table_02.json';
+import table03 from './table_03.json';
 
-const tables: TableJson[] = [table01];
+const tables: TableJson[] = [table01, table02, table03];
 
 /** Array of all available instances of `Table` class */
 export default tables.map(x => new Table(x));
