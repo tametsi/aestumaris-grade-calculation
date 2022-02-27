9110f1874e957471e99a061be3c2c565457b3d83
Vis17 <93092155+Vis17@users.noreply.github.com>
2022-02-27 16:50:38 +0100

    Refactor: extract calculation relevant code
    (in Calculation.svelte)

---
+++ src/pages/Calculation.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import CalculationForm from '../components/CalculationForm.svelte';
+	import Aestumaris from '../lib/aestumaris';
+
+	const aestumaris = new Aestumaris();
+
+	$: calculatedGrade = aestumaris.calculateGrade();
+	let tableIndex = 0;
+
+	$: {
+		aestumaris.tableManager.setActiveTable(tableIndex);
+		// update the calculatedGrade as well
+		calculatedGrade = aestumaris.calculateGrade();
+	}
+</script>
+
+<div class="wrapper">
+	<div class="card">
+		<section>
+			<h1>AESTUMARIS - Grade Calculation</h1>
+
+			<CalculationForm
+				bind:pointsMaximum={aestumaris.pointsMaximum}
+				bind:pointsReached={aestumaris.pointsReached}
+				tableList={aestumaris.tableManager.getTableList()}
+				bind:tableIndex
+			/>
+
+			<h2>Grade</h2>
+			<span class="grade">{calculatedGrade}</span>
+		</section>
+	</div>
+</div>
+
+<style lang="scss">
+	@use '../scss/abstracts' as a;
+
+	.wrapper {
+		padding: 1rem 0;
+
+		.card {
+			width: clamp(30rem, 75rem, 95%);
+			background-color: a.$gray5;
+			margin: auto;
+			padding: 1.5rem;
+
+			border-radius: 0.5rem;
+			box-shadow: darken(a.$gray6, 10) 0 0 1.5rem;
+
+			.grade {
+				font-size: 4rem;
+				background-color: a.$gray6;
+				border: 0.2rem solid darken(a.$gray1, 40);
+				border-radius: 0.4rem;
+				padding: 1rem 2rem;
+			}
+		}
+	}
+</style>
